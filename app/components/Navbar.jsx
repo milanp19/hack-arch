35a40c1e7448cca6b507a82025f5b736065af0d0
Goutham Krishna <99890612+gouthamk490@users.noreply.github.com>
2023-10-28 13:21:08 +0530

    updated successfully

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,21 +1,69 @@
 "use client";
 import Link from "next/link";
 import React from "react";
-import { authOptions } from "../api/auth/[...nextauth]/route";
 import { useSession } from "next-auth/react";
+import { AppBar, Button, Toolbar, Typography ,TextField,IconButton, InputBase} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
 
 const Navbar = () => {
   const { data: session } = useSession();
+  const customInputStyle = {
+    borderRadius: '10px'
+  };
+  const customcolourstyle = {
+    backgroundColor:'whitesmoke', borderRadius:"10px"
+  };
+  const customButtonStyle = {
+    fontSize: '16px',
+    textTransform: 'None',
+    marginRight:'27px',
+  };
 
   return (
-    <div>
+    <div >
+      <AppBar position="fixed"
+      style={{
+        boxSizing: "border-box",
+        background: "#131515",
+        // background: "rgba(255,255,255,0.2)",
+        height: "70px",
+        justifyContent:"center",
+      }}>
+      <Toolbar class="container" variant="dense"
+       style={{
+        marginLeft:"7px",
+        justifyContent: "space-between"
+        }}>
+        <div>
+        <Button variant="text" style={customButtonStyle} >
+          <Link style={{color: "#00A97F"}} href="/home">HOME</Link></Button>
+        <Button variant="text" style={customButtonStyle} >
+          <Link style={{color: "#00A97F"}} href="/about">ABOUT</Link></Button>
+          <Button variant="text" style={customButtonStyle} >
+          <Link style={{color: "#00A97F"}} href="/services">SERVICES</Link></Button>
+          <TextField class="textf" size='small' InputProps={{ style: customInputStyle , endAdornment: (
+          <InputAdornment position="end" >
+            <SearchIcon />
+          </InputAdornment>
+        ), }} style={customcolourstyle} placeholder='Search...'/>
+        </div>
       <h1>
         {!session ? (
-          <Link href="/api/auth/signin">Login</Link>
+        <div class="button__container">
+        <button  class="button" >
+          <Link href="/api/auth/signin">Login</Link></button>
+        <button  class="button" >
+         <Link href="/signup">SignUp</Link></button>
+        </div>
+        
         ) : (
-          <Link href="/api/auth/signout">Logout</Link>
+          <button>
+          <Link href="/api/auth/signout">Logout</Link></button>
         )}
       </h1>
+    </Toolbar>
+    </AppBar>
     </div>
   );
 };
---
+++ app/services/page.js
@@ -0,0 +1,125 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='page'>
+        <div className='design3'>
+        <div classname="card" >
+            <a class="card1" href="#">
+                <p><b>Expert Advice:</b></p>
+                <p class="small">Connect with retired professionals for advice on various topics related to your field.</p>
+                <div class="go-corner" href="#">
+                <div class="go-arrow">
+                    →
+                </div>
+                </div>
+            </a>
+        </div>
+        <div classname="card" >
+            <a class="card1" href="#">
+                <p><b>Personalized Training:</b></p>
+                <p class="small">Request specific training sessions from experts to enhance your skills.</p>
+                <div class="go-corner" href="#">
+                <div class="go-arrow">
+                    →
+                </div>
+                </div>
+            </a>
+        </div>
+        <div classname="card" >
+            <a class="card1" href="#">
+                <p><b>Networking:</b></p>
+                <p class="small">Connect and network with professionals from various fields.</p>
+                <div class="go-corner" href="#">
+                <div class="go-arrow">
+                    →
+                </div>
+                </div>
+            </a>
+        </div>
+        
+        <div classname="card" >
+            <a class="card1" href="#">
+                <p><b>Career Guidance:</b></p>
+                <p class="small"> Receive guidance on career progression and opportunities in your field.</p>
+                <div class="go-corner" href="#">
+                <div class="go-arrow">
+                    →
+                </div>
+                </div>
+            </a>
+        </div>
+        <div classname="card" >
+            <a class="card1" href="#">
+                <p><b>Task Evaluation:</b></p>
+                <p class="small">Get your work or tasks evaluated by experienced professionals for constructive feedback.</p>
+                <div class="go-corner" href="#">
+                <div class="go-arrow">
+                    →
+                </div>
+                </div>
+            </a>
+        </div>
+        
+        <div classname="card" >
+            <a class="card1" href="#">
+                <p><b>Earning Opportunity:</b></p>
+                <p class="small">By offering their services , they can earn additional income during their retirement.</p>
+                <div class="go-corner" href="#">
+                <div class="go-arrow">
+                    →
+                </div>
+                </div>
+            </a>
+        </div>
+        <div classname="card" >
+            <a class="card1" href="#">
+                <p><b>Connection with Professionals: </b></p>
+                <p class="small">Expertise helps to connect with professionals globally and share their wisdom with the next generation.</p>
+                <div class="go-corner" href="#">
+                <div class="go-arrow">
+                    →
+                </div>
+                </div>
+            </a>
+        </div>
+        <div classname="card" >
+            <a class="card1" href="#">
+                <p><b>Improving Public Profile:</b></p>
+                <p class="small">By sharing their knowledge and expertise on Expertise retired professionals can enhance their public profile.</p>
+                <div class="go-corner" href="#">
+                <div class="go-arrow">
+                    →
+                </div>
+                </div>
+            </a>
+        </div>
+        <div classname="card" >
+            <a class="card1" href="#">
+                <p><b>Premium Features:</b></p>
+                <p class="small">For those who subscribe to our premium services, we offer additional benefits such as featured profiles, priority booking, a premium badge and many more.</p>
+                <div class="go-corner" href="#">
+                <div class="go-arrow">
+                    →
+                </div>
+                </div>
+            </a>
+        </div>
+        <div classname="card" >
+            <a class="card1" href="#">
+                <p><b>Staying Updated with Technology:</b></p>
+                <p class="small">Expertise  platform is designed to be user-friendly and up-to-date with the latest technology trends thus making the  retired professionals stay connected</p>
+                <div class="go-corner" href="#">
+                <div class="go-arrow">
+                    →
+                </div>
+                </div>
+            </a>
+        </div>
+        </div>
+    </div>
+    
+  )
+}
+
+export default page
